22902d792a385096629c9afb2d1b77c9e897b546
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-11-08 09:27:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
  /*DIDITALE XD*/
  /*TURMINHA DO DIDI XD*/
- 
+#include "flecha_bmp.c"
 #include <reg52.h>
 #include "diditale_bmp.c"
-#include "..\lib\sll.h"
+#include "C:\Users\EduMaker\Downloads\diditale-main\diditale-main\sll.h"
 
 COLOR corShield = {0, 0, 255};
+COLOR eraseShield = {0,0,0};
  void main(void){
    unsigned char shieldx,shieldy;
    BYTE x = 100;
@@ -14,18 +15,23 @@ COLOR corShield = {0, 0, 255};
    paintscr(0, 0, 0);
    while(1){
 	 
-    for (shieldx = 90; shieldx <=131; shieldx++){
-      plotxy(shieldx,30,&corShield);
+    for (shieldx = 100; shieldx <=140; shieldx++){
+      plotxy(shieldx,44,&corShield);
     }
-     printbmp(120,64,diditale);
+     printbmp(110,54,diditale);
      if(vkeydown(VK_RIGHT)){
-
+			 for (shieldy = 44; shieldy <=84; shieldy++){
+				 plotxy(141,shieldy,&corShield);	
+			}
+			 plotxy(shieldx,44,&eraseShield);
      }
      if(vkeydown(VK_LEFT)){
 
      }
      if(vkeydown(VK_UP)){
-
+       for (shieldx = 100; shieldx <=140; shieldx++){
+         plotxy(shieldx,44,&corShield);
+			 }
      }
      if(vkeydown(VK_DOWN)){
 
